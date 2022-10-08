5e51cf8ee320ce0e9a76d8a81464a8647d3c5978
Ján Mareček <janomarecek2002@gmail.com>
2022-10-08 13:09:15 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*var nav_brand = $('navbar-brand');
+
+nav_brand.css({
+    position:'relative',
+    background:'yellow',
+})
+
+nav_brand.animate({top:1000},5000);*/
+
+$("document").ready(function (params) {
+    $("#technologies").scroll(function (params) {
+        $("#technologies").css("background-color","blue")
+    })
+    
+});
+
